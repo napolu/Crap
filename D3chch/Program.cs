c53d0ec517f393277edbde33695491e633a9f101
Natlia <napolu@yandex.ru>
2022-04-15 08:44:27 +0300

    Diabolic choice

---
+++ D3chch/Program.cs
@@ -0,0 +1,17 @@
+﻿int character = new Random().Next(1, 9);
+if(character == 1)
+    Console.WriteLine("Иди в монахи");
+if(character == 2)
+    Console.WriteLine("Чародей ко");
+if(character == 3)
+    Console.WriteLine("Барбар Елла");
+if(character == 4)
+    Console.WriteLine("Димона ханть");
+if(character == 5)
+    Console.WriteLine("Неси крестов");
+if(character == 6)
+    Console.WriteLine("Иди в колдунство");
+if(character == 7)
+    Console.WriteLine("Иди тропой мёртвых");
+if(character == 8)
+    Console.WriteLine("Иди нахер");
